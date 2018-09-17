8ca3c6a5aa003b9da2da7aa6f906f76a9b8a00ac
Nikolay Elovsky <echonok@gmail.com>
2018-09-17 22:16:43 +0300

    add

--- src/bin/gen-diff.js
+++ src/bin/gen-diff.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
-import run from '../index';
+import program from 'commander';
 
-run();
+export default () => {
+  program
+    .version('0.0.7')
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstArgument> <secondArgument>')
+    .option('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
+    .action(() => console.log('test message from commander.js'))
+    .parse(process.argv);
+};
