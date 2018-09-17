1db2359931916bb0f7933284eb477eb6d942d390
Nikolay Elovsky <echonok@gmail.com>
2018-09-17 22:31:04 +0300

    fix

--- src/bin/gen-diff.js
+++ src/bin/gen-diff.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
+
 import program from 'commander';
 
-export default () => {
-  program
-    .version('0.0.7')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<firstArgument> <secondArgument>')
-    .option('-h, --help', 'output usage information')
-    .option('-f, --format [type]', 'output format')
-    .action(() => console.log('test message from commander.js'))
-    .parse(process.argv);
-};
+program
+  .version('0.0.7')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstArgument> <secondArgument>')
+  .option('-h, --help', 'output usage information')
+  .action(() => console.log('test message from commander.js'))
+  .parse(process.argv);
