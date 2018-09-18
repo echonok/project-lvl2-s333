bdf669b80796508d92912263feeaea5122508715
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-09-18 22:53:01 +0300

    ~

--- __tests__/gen-fiff-test.js
+++ __tests__/gen-fiff-test.js
@@ -3,18 +3,14 @@ import path from 'path';
 import genDiff from '../src';
 
 describe('ProjestTests', () => {
-  it('#gen-diff JSON', () => {
-    
+  it('#gen-diff JSON', () => {    
     const file1 = '__tests__/__fixtures__/after.json';
     const file2 = '__tests__/__fixtures__/before.json';
     const actual = genDiff(file1, file2);
-
     const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected-json');
     console.log(__dirname);
     const rawData = fs.readFileSync(expectedFixturePath, 'utf-8');
     const expected = JSON.parse(rawData);
-  
     expect(actual).toEqual(expected);
-
   });
 });
