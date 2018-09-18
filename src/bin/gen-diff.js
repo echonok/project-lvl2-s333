48d186f5b561b03144d518b958be1a189f587991
Nick Elovsky <echonok@gmail.com>
2018-09-18 12:18:45 +0300

    fix gen-diff

--- src/bin/gen-diff.js
+++ src/bin/gen-diff.js
@@ -7,7 +7,5 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstArgument> <secondArgument>')
   .option('-f, --format [type]', 'Output format')
-  .action(function (firstArgument, secondArgument) {
-  	console.log(`${firstArgument} ${secondArgument} Test messages from commander.js`)
-  })
+  .action(console.log(`${firstArgument} ${secondArgument} Test messages from commander.js`))
   .parse(process.argv);
