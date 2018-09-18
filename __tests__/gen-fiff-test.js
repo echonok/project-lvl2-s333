f30f02584233f03518dc7205e136602509ebd1a8
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-09-18 22:18:21 +0300

    change test

--- __tests__/gen-fiff-test.js
+++ __tests__/gen-fiff-test.js
@@ -1,11 +1,11 @@
 import genDiff from '../src';
 
-test('gen-diff', () => {
+describe('gen-diff', () => {
   const file1 = '__tests__/__fixtures__/after.json';
   const file2 = '__tests__/__fixtures__/before.json';
 
   const actual = genDiff(file1, file2);
 
   const expected = '{\n\t- follow: false\n\t  host: hexlet.io\n\t- proxy: 123.234.53.22\n\t+ timeout: 20\n\t- timeout: 50\n\t+ verbose: true\n}';
-  expect(actual).toBe(expected);
+  expect(actual).toEqual(expected);
 });
