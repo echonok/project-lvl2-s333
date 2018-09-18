e311288082c7f0307c4097f95a8322c511f4ff8d
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-09-18 20:42:50 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -62,8 +62,4 @@ const genDiff = (path1, path2) => {
   return arrayToString(complexArrayToSimpleArray(result));
 };
 
-//const file1 = '__tests__/__fixtures__/after.json';
-//const file2 = '__tests__/__fixtures__/before.json';
-//console.log(genDiff(file1, file2));
-
 export default genDiff;
