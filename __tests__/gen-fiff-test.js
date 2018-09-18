212fae7dc0c1edcf87c7da8f64ccd9640727128f
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-09-18 22:55:58 +0300

    ~

--- __tests__/gen-fiff-test.js
+++ __tests__/gen-fiff-test.js
@@ -3,7 +3,7 @@ import path from 'path';
 import genDiff from '../src';
 
 describe('ProjestTests', () => {
-  it('#gen-diff JSON', () => {    
+  it('#gen-diff JSON', () => {
     const file1 = '__tests__/__fixtures__/after.json';
     const file2 = '__tests__/__fixtures__/before.json';
     const actual = genDiff(file1, file2);
