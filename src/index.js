34ff78bfcefb93f73bbec90aa4b45d0f6463d86e
Nick Elovsky <echonok@gmail.com>
2018-09-18 17:07:22 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import fs from 'fs';
 import _ from 'lodash';
 
-const genDiff = (path1, path2) => {
-  const data1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
-  const data2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const allKeys = _.union(keys1, keys2).sort();
-  const result = allKeys.reduce((acc, key) => {
-    const { changes, value } = compareValues(data1[key], data2[key]);
-    acc.push({ name: key, changes, value });
-    return acc;
-  }, []);
-  arrayToString(complexArrayToSimpleArray(result))
-};
-
 const compareValues = (value1, value2) => {
   if (value1 === value2) {
     return {
@@ -59,7 +45,21 @@ const arrayToString = someArray => {
     console.log(`  ${item.changes} ${item.name}: ${item.value}`);
   });
   console.log('}');
-}
+};
+
+const genDiff = (path1, path2) => {
+  const data1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
+  const data2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const allKeys = _.union(keys1, keys2).sort();
+  const result = allKeys.reduce((acc, key) => {
+    const { changes, value } = compareValues(data1[key], data2[key]);
+    acc.push({ name: key, changes, value });
+    return acc;
+  }, []);
+  arrayToString(complexArrayToSimpleArray(result))
+};
 
 const fileBefore = '__tests__/__fixtures__/before.json';
 const fileAfter = '__tests__/__fixtures__/after.json';
