af3741f9b799f8049c933385fd17556d0745a7fe
Nick Elovsky <echonok@gmail.com>
2018-09-19 14:21:00 +0300

    changed index

--- src/index.js
+++ src/index.js
@@ -9,25 +9,25 @@ const genDiff = (path1, path2) => {
   const fileExt = getExtension(path1);
   const data1 = parser(fileExt)(fs.readFileSync(path1, 'utf-8'));
   const data2 = parser(fileExt)(fs.readFileSync(path2, 'utf-8'));
-
-  const resultFirstStep = Object.keys(data1).reduce((acc, key) => {
-    if (_.has(data2, key)) {
-      if (data1[key] !== data2[key]) {
-        return `${acc}  + ${key}: ${data2[key]}\n  - ${key}: ${data1[key]}\n`;
-      }
-      return `${acc}    ${key}: ${data1[key]}\n`;
+  const allKeys = _.union(Object.keys(data1), Object.keys(data2));
+  const result = allKeys.reduce((acc, key) => {
+    if (!_.has(data2, key)) {
+      return `${acc}  - ${key}: ${data1[key]}\n`;
     }
-    return `${acc}  - ${key}: ${data1[key]}\n`;
+    if (!_.has(data1, key)) {
+      return `${acc}  + ${key}: ${data2[key]}\n`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `${acc}  + ${key}: ${data2[key]}\n  - ${key}: ${data1[key]}\n`;
+    }
+    return `${acc}    ${key}: ${data1[key]}\n`;
   }, '');
-
-  const resultSecondStep = Object.keys(data2).reduce((acc, key) => {
-    if (_.has(data1, key)) {
-      return acc;
-    }
-    return `${acc}  + ${key}: ${data2[key]}\n`;
-  }, resultFirstStep);
-
-  return `{\n${resultSecondStep}}`;
+  return `{\n${result}}`;
 };
+
+const file1 = 'src/before.json';
+const file2 = 'src/after.json';
+
+console.log(genDiff(file1, file2));
 
 export default genDiff;
--- src/parsers/parsers.js
+++ src/parsers/parsers.js
@@ -1,9 +1,9 @@
-import ini from 'ini';
+import inip from 'ini';
 import yaml from 'js-yaml';
 
-const parserJSON = objectForParcing => JSON.parse(objectForParcing);
-const parserYML = objectForParcing => yaml.safeLoad(objectForParcing);
-const parserINI = objectForParcing => ini.parse(objectForParcing);
-const parsers = { parserJSON, parserYML, parserINI };
+const json = objectForParcing => JSON.parse(objectForParcing);
+const yml = objectForParcing => yaml.safeLoad(objectForParcing);
+const ini = objectForParcing => inip.parse(objectForParcing);
+const parsers = { json, yml, ini };
 
 export default extension => parsers[extension];
