c45c3015e01cf68d2cc5ad8be1cef0ae8be376b7
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-09-19 07:33:30 +0300

    changed gendif

--- src/index.js
+++ src/index.js
@@ -1,69 +1,31 @@
 import fs from 'fs';
 import _ from 'lodash';
 
-const compareValues = (value1, value2) => {
-  if (value1 === value2) {
-    return {
-      changes: ' ',
-      value: value1,
-    };
-  }
-  if (value1 === undefined) {
-    return {
-      changes: '-',
-      value: value2,
-    };
-  }
-  if (value2 === undefined) {
-    return {
-      changes: '+',
-      value: value1,
-    };
-  }
-  return {
-    changes: '-+',
-    value: [value1, value2],
-  };
-};
-
-const complexArrayToSimpleArray = (someArray) => {
-  const finalArray = someArray.reduce((acc, record) => {
-    if (typeof record.value === 'object') {
-      acc.push({ name: record.name, changes: '+', value: record.value[0] });
-      acc.push({ name: record.name, changes: '-', value: record.value[1] });
-    } else {
-      acc.push(record);
-    }
-    return acc;
-  }, []);
-  return finalArray;
-};
-
-const arrayToString = (someArray) => {
-  let someString = '{';
-  someArray.forEach((item) => {
-    someString += `\n  ${item.changes} ${item.name}: ${item.value}`;
-  });
-  someString += '\n}';
-  return someString;
-};
-
 const genDiff = (path1, path2) => {
   const data1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
   const data2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const allKeys = _.union(keys1, keys2).sort();
-  const result = allKeys.reduce((acc, key) => {
-    const { changes, value } = compareValues(data1[key], data2[key]);
-    acc.push({ name: key, changes, value });
-    return acc;
-  }, []);
-  return arrayToString(complexArrayToSimpleArray(result));
-};
 
-const file1 = '__tests__/__fixtures__/after.json';
-const file2 = '__tests__/__fixtures__/before.json';
-genDiff(file1, file2);
+  const resultFirstStep = Object.keys(data1).reduce((acc, key) => {
+    if (_.has(data2, key)) {
+      if (data1[key] !== data2[key]) {
+        return `${acc}  + ${key}: ${data2[key]}\n  - ${key}: ${data1[key]}\n`;
+      } else {
+        return `${acc}    ${key}: ${data1[key]}\n`;
+      }
+    } else {
+      return `${acc}  - ${key}: ${data1[key]}\n`;
+    }
+  }, '');
+
+  const resultSecondStep = Object.keys(data2).reduce((acc, key) => {
+    if (_.has(data1, key)) {
+      return acc;
+    } else {
+      return `${acc}  + ${key}: ${data2[key]}\n`
+    }
+  }, resultFirstStep);
+
+  return `{\n${resultSecondStep}}`;
+};
 
 export default genDiff;
