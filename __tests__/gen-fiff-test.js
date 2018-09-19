3a4263dfe203cb4128c5aec8062a5676bad2065f
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-09-19 07:46:06 +0300

    fix test

--- __tests__/gen-fiff-test.js
+++ __tests__/gen-fiff-test.js
@@ -4,8 +4,8 @@ import genDiff from '../src';
 
 describe('ProjestTests', () => {
   it('#gen-diff JSON', () => {
-    const file1 = path.join(__dirname, '__fixtures__', 'after.json');
-    const file2 = path.join(__dirname, '__fixtures__', 'before.json');
+    const file1 = path.join(__dirname, '__fixtures__', 'before.json');
+    const file2 = path.join(__dirname, '__fixtures__', 'after.json');
     const actual = genDiff(file1, file2);
     const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected-json');    
     const expected = fs.readFileSync(expectedFixturePath, 'utf-8');
