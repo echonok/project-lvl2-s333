50ff67bc2574f30ad7bccb22441c0a6b037b33fd
Nick Elovsky <echonok@gmail.com>
2018-09-19 12:49:02 +0300

    fix

--- src/parsers/parsers.js
+++ src/parsers/parsers.js
@@ -1,10 +1,9 @@
 import ini from 'ini';
 import yaml from 'js-yaml';
 
-const parsers = { parserJSON, parserYML, parserINI };
-
 const parserJSON = objectForParcing => JSON.parse(objectForParcing);
 const parserYML = objectForParcing => yaml.safeLoad(objectForParcing);
 const parserINI = objectForParcing => ini.parse(objectForParcing);
+const parsers = { parserJSON, parserYML, parserINI };
 
 export default extension => parsers[extension];
