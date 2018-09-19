cbff0ec3a04e0dfbeae676b6f61d6c194bb5328d
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-09-19 20:07:38 +0300

    ~

--- src/bin/gen-diff.js
+++ src/bin/gen-diff.js
@@ -9,5 +9,5 @@ program
   .description(description )
   .arguments('<firstArgument> <secondArgument>')
   .option('-f, --format [type]', 'Output format')
-  .action((path1, path2) => console.log(genDiff(path1, path2)));
+  .action((path1, path2) => console.log(genDiff(path1, path2)))
   .parse(process.argv);
