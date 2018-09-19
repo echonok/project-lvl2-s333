33f71ab264dc359be0e1cd78fd38adbffd28484f
Nick Elovsky <echonok@gmail.com>
2018-09-19 12:55:21 +0300

    fix

--- __tests__/gen-diff-test.js
+++ __tests__/gen-diff-test.js
@@ -7,7 +7,7 @@ describe('ProjestTests', () => {
     const path1 = path.join(__dirname, '__fixtures__', 'before.json');
     const path2 = path.join(__dirname, '__fixtures__', 'after.json');
     const actual = genDiff(path1, path2);
-    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');    
+    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');
     const expected = fs.readFileSync(expectedFixturePath, 'utf-8');
     expect(actual).toEqual(expected);
   });
@@ -16,7 +16,7 @@ describe('ProjestTests', () => {
     const path1 = path.join(__dirname, '__fixtures__', 'before.yml');
     const path2 = path.join(__dirname, '__fixtures__', 'after.yml');
     const actual = genDiff(path1, path2);
-    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');    
+    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');
     const expected = fs.readFileSync(expectedFixturePath, 'utf-8');
     expect(actual).toEqual(expected);
   });
@@ -25,7 +25,7 @@ describe('ProjestTests', () => {
     const path1 = path.join(__dirname, '__fixtures__', 'before.ini');
     const path2 = path.join(__dirname, '__fixtures__', 'after.ini');
     const actual = genDiff(path1, path2);
-    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');    
+    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');
     const expected = fs.readFileSync(expectedFixturePath, 'utf-8');
     expect(actual).toEqual(expected);
   });
