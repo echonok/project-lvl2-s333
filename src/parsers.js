8b7442f85231616fa0ec45c91bf70a770ec5d415
Nick Elovsky <echonok@gmail.com>
2018-09-19 17:57:05 +0300

    ~

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,10 @@
 import inip from 'ini';
 import yaml from 'js-yaml';
 
-const parsers = { 
-	json: JSON.parse(objectForParcing), 
-	yml: yaml.safeLoad(objectForParcing),
-	ini: inip.parse(objectForParcing,
+const parsers = {
+  json: JSON.parse(objectForParcing),
+  yml: yaml.safeLoad(objectForParcing),
+  ini: inip.parse(objectForParcing,
 };
 
 export default extension => parsers[extension];
