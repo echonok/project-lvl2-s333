5aec8f51e00900d16469cb79ed7000b285a48e26
Nick Elovsky <echonok@gmail.com>
2018-09-20 11:45:34 +0300

    ~

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const compareTwoData = (data1, data2, repeater = 1) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   const result = keys.reduce((acc, key) => {
 
-
     if (data1[key] instanceof Object && data2[key] instanceof Object) {
       if (!_.has(data2, key)) {
         return [...acc, `${'  '.repeat(repeater)}- ${key}: ${compareTwoData(data1[key], data2[key], repeater + 2)}`];
@@ -47,9 +46,7 @@ const compareTwoData = (data1, data2, repeater = 1) => {
     }
 
     return [...acc, `${'  '.repeat(repeater)}  ${key}: ${data1[key]}`];
-
   }, []);
-
   const bracketSpace = repeater === 1 ? '' : ' ';
   return `{\n${_.flatten(result).join('\n')}\n${bracketSpace.repeat(repeater + 1)}}`;
 };
@@ -60,6 +57,7 @@ const genDiff = (path1, path2) => {
   const file2 = fs.readFileSync(path2, 'utf-8');
   const data1 = parser(fileExt, file1);
   const data2 = parser(fileExt, file2);
+  compareTwoData(data1, data2);
 };
 
 export default genDiff;
