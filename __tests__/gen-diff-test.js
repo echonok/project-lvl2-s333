ccd4ad92cdfc5caf57751128834aadee3a285671
Nick Elovsky <echonok@gmail.com>
2018-09-20 11:50:34 +0300

    add compex_expected

--- __tests__/gen-diff-test.js
+++ __tests__/gen-diff-test.js
@@ -2,7 +2,36 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-describe('ProjestTests', () => {
+describe('Simle Tests', () => {
+  it('#gen-diff JSON', () => {
+    const path1 = path.join(__dirname, '__fixtures__', 'before.json');
+    const path2 = path.join(__dirname, '__fixtures__', 'after.json');
+    const actual = genDiff(path1, path2);
+    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');
+    const expected = fs.readFileSync(expectedFixturePath, 'utf-8');
+    expect(actual).toEqual(expected);
+  });
+
+  it('#gen-diff YAML', () => {
+    const path1 = path.join(__dirname, '__fixtures__', 'before.yml');
+    const path2 = path.join(__dirname, '__fixtures__', 'after.yml');
+    const actual = genDiff(path1, path2);
+    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');
+    const expected = fs.readFileSync(expectedFixturePath, 'utf-8');
+    expect(actual).toEqual(expected);
+  });
+
+  it('#gen-diff INI', () => {
+    const path1 = path.join(__dirname, '__fixtures__', 'before.ini');
+    const path2 = path.join(__dirname, '__fixtures__', 'after.ini');
+    const actual = genDiff(path1, path2);
+    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');
+    const expected = fs.readFileSync(expectedFixturePath, 'utf-8');
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('Complex Tests', () => {
   it('#gen-diff JSON', () => {
     const path1 = path.join(__dirname, '__fixtures__', 'before.json');
     const path2 = path.join(__dirname, '__fixtures__', 'after.json');
