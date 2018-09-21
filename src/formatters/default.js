772f75d273b06f73ee14fed2c4e617d960e4b3da
Nikolay Elovsky <echonok@users.noreply.github.com>
2018-09-21 07:23:53 +0300

    add more

---
+++ src/formatters/default.js
@@ -0,0 +1,52 @@
+import _ from 'lodash';
+
+const tabSymbol = '  ';
+
+const stringify = (value, repeater = 1) => {
+  if (!_.isObject(value)) {
+    return `${value}`;
+  }
+
+  const firstSpace = tabSymbol.repeat(repeater + 1);
+
+  const keys = Object.keys(value).map(key => {
+    return `${firstSpace}${key}: ${stringify(value[key], repeater + 2)}`;
+  });
+
+  return `{\n${_.flatten(keys).join('\n')}\n${tabSymbol.repeat(repeater)}}`;
+};
+
+const valueToString = (key, value, tabsCount) => {
+  return `${key}: ${stringify(value, tabsCount)}`;
+};
+
+const renderDifferences = (differences, repeater = 1) => {
+  const firstSpace = tabSymbol.repeat(repeater);
+
+  const arr = differences.map((element) => {
+    
+    const { name, type, value1, value2, children } = element;
+
+    switch (type) {
+      case 'object':
+        return `${firstSpace}  ${name}: ${renderDifferences(children, repeater + 2)}`;
+      case 'unchanged':
+        return `${firstSpace}  ${valueToString(name, value1, repeater + 2)}`;
+      case 'added':
+        return `${firstSpace}+ ${valueToString(name, value2, repeater + 1)}`;
+      case 'deleted':
+        return `${firstSpace}- ${valueToString(name, value1, repeater + 1)}`;
+      case 'changed':
+        return [
+          `${firstSpace}+ ${valueToString(name, value2, repeater + 1)}`,
+          `${firstSpace}- ${valueToString(name, value1, repeater + 1)}`,
+        ];
+      default:
+        return;
+    }
+  });
+
+  return `{\n${_.flatten(arr).join('\n')}\n${tabSymbol.repeat(repeater - 1)}}`;
+};
+
+export default renderDifferences;
--- src/index.js
+++ src/index.js
@@ -2,125 +2,46 @@ import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
 import parser from './parsers';
+import formatters from './formatters';
 
 const getExtension = pathToFile => path.extname(`${pathToFile}`).replace(/\./g, '');
 
-const compareTwoData = (obj1, obj2) => {
-  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
+const compareTwoData = (data1, data2) => {
+  return _.union(_.keys(data1), _.keys(data2)).map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
 
-  const compareValues = (value1, value2) => {
-    if (typeof value1 === 'object' && typeof value2 === 'object') {
-      return {
-        type: 'unchanged',
-        data: compareTwoData(value1, value2),
-      };
+    if (!_.has(data2, key)) {
+      return { name: key, type: 'deleted', value1 };
+    }
+
+    if (!_.has(data1, key)) {
+      return { name: key, type: 'added', value2 };
+    }
+
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { name: key, type: 'object', children: compareTwoData(value1, value2) };
     }
 
     if (value1 === value2) {
-      return {
-        type: 'unchanged',
-        data: value1,
-      };
+      return { name: key, type: 'unchanged', value1 };
     }
 
-    if (typeof value1 === 'object' && value2 === undefined) {
-      return {
-        type: 'deleted',
-        data: compareTwoData(value1, value1),
-      };
-    }
-    if (value1 === undefined && typeof value2 === 'object') {
-      return {
-        type: 'created',
-        data: compareTwoData(value2, value2),
-      };
-    }
-
-    if (typeof value1 === 'object' && value2 !== undefined) {
-      return {
-        type: 'changedOld',
-        data: [compareTwoData(value1, value1), value2],
-      };
-    }
-    if (value1 !== undefined && typeof value2 === 'object') {
-      return {
-        type: 'changedNew',
-        data: [value1, compareTwoData(value2, value2)],
-      };
-    }
-
-    if (value1 === undefined) {
-      return {
-        type: 'created',
-        data: value2,
-      };
-    }
-    if (value2 === undefined) {
-      return {
-        type: 'deleted',
-        data: value1,
-      };
-    }
     return {
-      type: 'changed',
-      data: [value1, value2],
+      name: key, type: 'changed', value1, value2,
     };
-  };
-
-  const newArr = keys.reduce((acc, key) => {
-    const { type, data } = compareValues(obj1[key], obj2[key]);
-    acc.push({ name: key, type, data });
-    return acc;
-  }, []);
-  return newArr;
+  })
 };
 
-
-const stringify = (differences, repeater = 1) => {
-  const result = differences.reduce((acc, key) => {
-    if (typeof key.data === 'object') {
-      switch (key.type) {
-        case 'unchanged':
-          return [...acc, `${'  '.repeat(repeater)}  ${key.name}: ${stringify(key.data, repeater + 2)}`];
-        case 'changed':
-          return [...acc, [`${'  '.repeat(repeater)}- ${key.name}: ${key.data[0]}`, `${'  '.repeat(repeater)}+ ${key.name}: ${key.data[1]}`]];
-        case 'changedOld':
-          return [...acc, [`${'  '.repeat(repeater)}- ${key.name}: ${stringify(key.data[0], repeater + 2)}`, `${'  '.repeat(repeater)}+ ${key.name}: ${key.data[1]}`]];
-        case 'changedNew':
-          return [...acc, [`${'  '.repeat(repeater)}- ${key.name}: ${key.data[0]}`, `${'  '.repeat(repeater)}+ ${key.name}: ${stringify(key.data[1], repeater + 2)}`]];
-        case 'created':
-          return [...acc, `${'  '.repeat(repeater)}+ ${key.name}: ${stringify(key.data, repeater + 2)}`];
-        case 'deleted':
-          return [...acc, `${'  '.repeat(repeater)}- ${key.name}: ${stringify(key.data, repeater + 2)}`];
-        default:
-          return acc;
-      }
-    }
-    switch (key.type) {
-      case 'unchanged':
-        return [...acc, `${'  '.repeat(repeater)}  ${key.name}: ${key.data}`];
-      case 'changed':
-        return [...acc, [`${'  '.repeat(repeater)}- ${key.name}: ${key.data[0]}`, `${'  '.repeat(repeater)}+ ${key.name}: ${key.data[1]}`]];
-      case 'created':
-        return [...acc, `${'  '.repeat(repeater)}+ ${key.name}: ${key.data}`];
-      case 'deleted':
-        return [...acc, `${'  '.repeat(repeater)}- ${key.name}: ${key.data}`];
-      default:
-        return acc;
-    }
-  }, []);
-  const bracketSpace = repeater === 1 ? '' : ' ';
-  return `{\n${_.flatten(result).join('\n')}\n${bracketSpace.repeat((repeater * 2) - 2)}}`;
-};
-
-const genDiff = (path1, path2) => {
+const genDiff = (path1, path2, keys) => {
   const fileExt = getExtension(path1);
   const file1 = fs.readFileSync(path1, 'utf-8');
   const file2 = fs.readFileSync(path2, 'utf-8');
   const data1 = parser(fileExt, file1);
   const data2 = parser(fileExt, file2);
-
-  return (stringify(compareTwoData(data1, data2)));
+  const differences = compareTwoData(data1, data2);
+  const finalRender = formatters(keys.format)(differences);
+  return finalRender;
 };
 
 export default genDiff;
